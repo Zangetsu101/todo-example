806d91f5c041dd026a2c6ed2a887d5d0dbf19f6a
tasnuvatuba <tasnuvatuba99@gmail.com>
2024-04-02 16:06:43 +0600

    feat: initial post, put, patch, delete

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,6 +1,7 @@
 import { Elysia, t } from 'elysia'
+let globalId = 3
 
-const TODOS = [
+let TODOS = [
   {
     id: 1,
     starred: false,
@@ -30,8 +31,58 @@ const app = new Elysia()
       params: t.Object({
         id: t.Numeric()
       })
-    }
-  )
+    })
+    .delete('/todos/delete/:id', ({params, error}) => {
+        const todoId = parseInt(params.id);
+        const todos = TODOS.filter((todo) => todo.id !== todoId)
+        if (TODOS.length == todos.length){
+            return error(404)
+        }
+        TODOS = todos
+        return TODOS
+    })
+    .post('/todos', ({ params, error, body }) => {
+        if (body && body.desc) {
+            const newTodo = {
+                id: globalId,
+                desc: body.desc,
+                starred: body.starred || false,
+                completed: body.completed || false
+            };
+            globalId++; // Increment globalId for the next todo
+            TODOS.push(newTodo);
+            return TODOS;
+        }
+        return error(404, 'Todo description is required.');
+
+    })
+    .put('/todos/:id', ({params, body, error}) => {
+        const todoId = parseInt(params.id);
+        const index = TODOS.findIndex(todo => todo.id === todoId);
+        if (index === -1) {
+            return error(404, 'Todo not found');
+        }
+        const updatedTodo = {
+            ...TODOS[index],
+            ...body
+        };
+        TODOS[index] = updatedTodo;
+        return TODOS
+    })
+    .patch('/todos/:id', ({ params, body, error }) => {
+        const todoId = parseInt(params.id);
+        const index = TODOS.findIndex(todo => todo.id === todoId);
+        if (index === -1) {
+            return error(404, 'Todo not found');
+        }
+        const updatedTodo = {
+            ...TODOS[index],
+            ...body
+        };
+        TODOS[index] = updatedTodo;
+        return TODOS[index];
+    })
+
   .listen(3000)
 
 console.log(
