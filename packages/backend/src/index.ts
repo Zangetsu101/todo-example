fb9b238764b9ff93c5dd0fc047f5c82f8da83838
Abdur Rakib <rakib.cse14th@gmail.com>
2024-04-02 19:50:53 +0600

    feat: update, delete, add functionality added.

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,6 +1,6 @@
 import { Elysia, t } from 'elysia'
 
-const TODOS = [
+let TODOS = [
   {
     id: 1,
     starred: false,
@@ -15,8 +15,48 @@ const TODOS = [
   }
 ]
 
+/*
+ * GET /todos
+ * POST /todos
+ * GET /todos/123421
+ * PUT /todos/1234321 {}
+ * PATCH /todos/12312312 {}
+ * DELETE /todos/1231231
+ */
+
 const app = new Elysia()
   .get('/todos', () => TODOS)
+  .post(
+    '/todos',
+    ({ body, error }) => {
+      const newTodo = { id: TODOS.length + 1, ...body }
+      try {
+        TODOS = [...TODOS, newTodo]
+        return {
+          status: 200,
+          message: 'success',
+          data: newTodo
+        }
+      } catch (e) {
+        return error(500)
+      }
+    },
+    {
+      body: t.Object({
+        starred: t.Boolean({
+          error: 'Starred must be boolean.'
+        }),
+        completed: t.Boolean({
+          error: 'Completed must be boolean.'
+        }),
+        desc: t.String({
+          minLength: 3,
+          maxLength: 255,
+          error: 'Description must be 3-255 characters.'
+        })
+      })
+    }
+  )
   .get(
     '/todos/:id',
     ({ params, error }) => {
@@ -32,16 +72,110 @@ const app = new Elysia()
       })
     }
   )
+  .put(
+    '/todos/:id',
+    ({ params, body, error }) => {
+      try {
+        const existing = { id: params.id, ...body }
+        TODOS = TODOS.map((todo) => {
+          if (todo.id == params.id) return existing
+          return todo
+        })
+        return {
+          status: 200,
+          message: 'success',
+          data: existing
+        }
+      } catch (e) {
+        return error(500)
+      }
+    },
+    {
+      params: t.Object({
+        id: t.Numeric()
+      }),
+      body: t.Object({
+        starred: t.Boolean({
+          error: 'Starred must be boolean.'
+        }),
+        completed: t.Boolean({
+          error: 'Completed must be boolean.'
+        }),
+        desc: t.String({
+          minLength: 3,
+          maxLength: 255,
+          error: 'Description must be 3-255 characters.'
+        })
+      })
+    }
+  )
+  .patch(
+    '/todos/:id',
+    ({ params, body, error }) => {
+      try {
+        let existing = TODOS.find((todo) => params.id == todo.id)
+
+        if (!existing) {
+          return error(404, {
+            message: 'error'
+          })
+        }
+
+        existing = { ...existing, completed: body.completed }
+
+        TODOS = TODOS.map((todo) => {
+          if (todo.id == params.id) return existing
+          return todo
+        })
+        return {
+          status: 200,
+          message: 'success',
+          data: existing
+        }
+      } catch (e) {
+        return error(500)
+      }
+    },
+    {
+      params: t.Object({
+        id: t.Numeric()
+      }),
+      body: t.Object({
+        completed: t.Boolean({
+          error: 'Completed must be boolean.'
+        })
+      })
+    }
+  )
+  .delete(
+    '/todos/:id',
+    ({ params, error }) => {
+      try {
+        const existing = TODOS.find((todo) => params.id == todo.id)
+
+        if (!existing) {
+          return error(404, {
+            message: 'error'
+          })
+        }
+        TODOS = TODOS.filter((todo) => todo.id != params.id)
+        return {
+          status: 200,
+          message: 'success'
+        }
+      } catch (e) {
+        return error(500)
+      }
+    },
+    {
+      params: t.Object({
+        id: t.Numeric()
+      })
+    }
+  )
+
   .listen(3000)
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 )
-/*
- * GET /todos
- * GET /todos/123421
- * POST /todos
- * PUT /todos/1234321 {}
- * PATCH /todos/12312312 {}
- * DELETE /todos/1231231
- */
