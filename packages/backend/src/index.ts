c3c11fe60fa9173c482a36662d20247456635633
Tameem Bin Haider <haidertameem@gmail.com>
2024-04-02 11:55:02 +0600

    feat: implement GET todos endpoint

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,7 +1,47 @@
-import { Elysia } from 'elysia'
+import { Elysia, t } from 'elysia'
 
-const app = new Elysia().get('/', () => 'Hello Elysia').listen(3000)
+const TODOS = [
+  {
+    id: 1,
+    starred: false,
+    completed: false,
+    desc: 'Wake up at 5am'
+  },
+  {
+    id: 2,
+    starred: false,
+    completed: false,
+    desc: 'Brush your teeth'
+  }
+]
+
+const app = new Elysia()
+  .get('/todos', () => TODOS)
+  .get(
+    '/todos/:id',
+    ({ params, error }) => {
+      const todo = TODOS.find((todo) => todo.id === params.id)
+      if (!todo) {
+        return error(404)
+      }
+      return todo
+    },
+    {
+      params: t.Object({
+        id: t.Numeric()
+      })
+    }
+  )
+  .listen(3000)
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 )
+/*
+ * GET /todos
+ * GET /todos/123421
+ * POST /todos
+ * PUT /todos/1234321 {}
+ * PATCH /todos/12312312 {}
+ * DELETE /todos/1231231
+ */
