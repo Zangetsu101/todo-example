52de6b83fc58eea535941694785a4d085cea0535
Tameem Bin Haider <haidertameem@gmail.com>
2024-04-23 12:24:55 +0600

    fix: add autoincrement to id column

--- packages/backend/src/db/schema.ts
+++ packages/backend/src/db/schema.ts
@@ -1,7 +1,7 @@
 import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core'
 
 export const todos = sqliteTable('todos', {
-  id: integer('id').primaryKey(),
+  id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
   starred: integer('id', { mode: 'boolean' }),
   completed: integer('id', { mode: 'boolean' }),
   desc: text('desc')
